d8f1511581640ab7b53b2b9831c5eaa5ade44f7d
wallace-y <cameronwfinney@gmail.com>
2023-05-31 11:33:11 +0100

    updating styling adding like button limit to 1

--- src/Components/CommentList.jsx
+++ src/Components/CommentList.jsx
@@ -5,6 +5,7 @@ import { getCommentByReviewId } from "../utils";
 import moment from "moment";
 import { addComment } from "../utils";
 import Error from "./Error";
+import CommentCard from "./CommentCard";
 
 function CommentList() {
   const { review_id } = useParams();
@@ -116,30 +117,7 @@ function CommentList() {
       </form>
       <ul className="list-group ">
         {comments.map((comment) => {
-          const formattedDate = moment(comment.created_at).format("D MMM YYYY");
-
-          return (
-            <li
-              key={comment.comment_id}
-              className="list-group-item d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">
-                  {comment.author}, {formattedDate}
-                </div>
-                {comment.body}
-              </div>
-              <button>
-                <i className="fa-solid fa-thumbs-up btn"></i>
-              </button>
-              <button>
-                <i className="fa-solid fa-trash btn"></i>
-              </button>
-              <span className="badge bg-primary rounded-pill position-absolute top-0 start-100 translate-middle">
-                {comment.votes}
-              </span>
-            </li>
-          );
+          return <CommentCard key={comment.comment_id} comment={comment} />;
         })}
       </ul>
     </main>
