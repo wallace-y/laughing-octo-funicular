0ae97886d032c3013310f7fd6122635f5d1782bc
wallace-y <cameronwfinney@gmail.com>
2023-06-01 10:31:04 +0100

    adding avatar_url to the navbar

--- src/Components/CurrentUser.jsx
+++ src/Components/CurrentUser.jsx
@@ -1,10 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/User";
+import { getUser } from "../utils";
 
 const CurrentUser = () => {
   const { user } = useContext(UserContext);
+  const [avatar, setAvatar] = useState("");
 
-  return <p className="text-light my-0">You're logged in as: {user}</p>;
+  useEffect(() => {
+    getUser(user).then((res) => {
+      setAvatar(res.avatar_url);
+    }).catch((err) => {
+      console.log(err)
+    });
+  }, []);
+
+  return (
+    <p className="text-light my-0">
+      You're logged in as: {user}{" "}
+      <img
+      className="img-thumbnail shadow"
+        src={`${avatar}`}
+        alt="user avatar logo"
+        style={{ clipPath: "circle()", width: "50px" }}
+      ></img>
+    </p>
+  );
 };
 
 export default CurrentUser;
